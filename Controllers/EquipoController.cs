3d00f9adc3af968a216ba9fe7cf8d41adb06b948
Mateo Cisneros <mateo.cisneros@udla.edu.ec>
2025-04-11 22:54:24 -0500

    Controller Creado

--- Controllers/EquipoController.cs
+++ Controllers/EquipoController.cs
@@ -1,6 +1,32 @@
+using Cisneros_LigaPro.Models;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Cisneros_LigaPro.Controllers;
 
-public class EquipoController
+public class EquipoController : Controller
 {
-    
+    private static List<Equipo> Equipos = new List<Equipo>
+    {
+        new Equipo { Nombre = "Equipo A", PartidosJugados = 5, PartidosGanados = 3, PartidosEmpatados = 1, PartidosPerdidos = 1 },
+        new Equipo { Nombre = "Equipo B", PartidosJugados = 5, PartidosGanados = 2, PartidosEmpatados = 2, PartidosPerdidos = 1 }
+    };
+
+    public IActionResult TablaPosiciones()
+    {
+        return View(Equipos);
+    }
+
+    [HttpPost]
+    public IActionResult ActualizarEquipo(Equipo equipo)
+    {
+        var equipoExistente = Equipos.FirstOrDefault(e => e.Nombre == equipo.Nombre);
+        if (equipoExistente != null)
+        {
+            equipoExistente.PartidosJugados = equipo.PartidosJugados;
+            equipoExistente.PartidosGanados = equipo.PartidosGanados;
+            equipoExistente.PartidosEmpatados = equipo.PartidosEmpatados;
+            equipoExistente.PartidosPerdidos = equipo.PartidosPerdidos;
+        }
+        return RedirectToAction("TablaPosiciones");
+    }
 }
--- Models/Equipo.cs
+++ Models/Equipo.cs
@@ -1,11 +1,27 @@
 namespace Cisneros_LigaPro.Models;
 
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
 public class Equipo
 {
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    [DisplayName("Nombre del equipo")]
     public string Nombre { get; set; }
-    public int Puntos { get; set; }
+
+    [Range(0, 100)]
     public int PartidosJugados { get; set; }
-    public int Ganados { get; set; }
-    public int Empatados { get; set; }
-    public int Perdidos { get; set; }
+
+    [Range(0, 100)]
+    public int PartidosGanados { get; set; }
+
+    [Range(0, 100)]
+    public int PartidosEmpatados { get; set; }
+
+    [Range(0, 100)]
+    public int PartidosPerdidos { get; set; }
 }
